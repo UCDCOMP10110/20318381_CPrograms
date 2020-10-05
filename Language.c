aec692f4c184b4b2ed7ad1746a9a067868858f97
Brian Monoranu <Brian.monoranu@ucdconnect.com>
2020-10-05 15:40:01 +0100

    Lab quiz 1

---
+++ Language.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(void)
+{
+  printf("Hello\t\tWorld!!\n");
+  printf("Goodbye\t\tWorld!!\n");
+
+
+  printf("o");
+  printf("n");
+  printf("e");
+  printf("\n");
+  printf("t");
+  printf("w");
+  printf("o");
+  printf("\n");
+
+
+  printf("one\n");
+  printf("two\n");
+  printf("three\n");
+
+
+  printf("one\n two\n");
+
+  return 0;
+}
