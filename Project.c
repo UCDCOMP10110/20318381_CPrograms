e8b7c2cb81239d1549184ea71cbd510e206db2cf
Brian Monoranu <Brian.monoranu@ucdconnect.com>
2020-10-10 09:40:46 +0100

    Any integer multiplied by 7

---
+++ Project.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+  /* declare some varibles*/
+  int f;
+
+  printf("Enter in an integer here:    ");
+  scanf("%d", &f);
+  printf("This integer multiplied by seven is %d \n", f*7);
+  return 0;
+  
+}
