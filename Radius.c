6862114b30805cd47ac98e0f7f77a310a52d34be
Brian Monoranu <Brian.monoranu@ucdconnect.com>
2020-10-12 19:00:16 +0100

    Lab quiz Q2

---
+++ Radius.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(void)
+{
+  double r, area;
+
+printf("Please enter the radius of the circle  ");
+scanf("%lf", &r);
+
+area= 3.14*r*r;
+
+printf("This is the radius of a circle %lf\n", area);
+return 0;
+
+
+
+}
