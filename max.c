bf5a4d94dc5350955495f1094b63b8629783cfc3
Brian Monoranu <Brian.monoranu@ucdconnect.com>
2020-11-02 13:04:23 +0000

    Selecting the max

---
+++ max.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+
+int main(void) {
+    double first,second,third,four;
+
+    double w, x, y, z;
+    /* you may need some extra variables - but NO arrays please! */
+
+    /* Do not change the following input statement */
+    scanf("%lf%lf%lf%lf", &w,&x,&y,&z);
+
+    /* Enter your solution here */
+
+  if((z<=x)&&(z<=y)&&(z<=w)){
+    four = z;
+  }
+
+  if(((z>=w) && (z>=x) && (z<=y)) || ((z>=w) && (z<=x) && (z>=y)) || ((z<=w) && (z>=x) && (z>=y)))
+  {
+     second = z;
+  }
+
+  if((z>=x)&&(z>=y)&&(z>=w)){
+    first= z;
+  }
+
+  if(((z>=w)&&(w<=x)&&(w<=y)) || ((w<= z)&& (w>=x)&&(w<=y)) || ((w<=z)&&(w<=x)&&(w>=y)))
+  {
+      third = z;
+  }
+
+
+  if(((w>=z)&&(w<=x)&&(w<=y)) || ((w<= z)&& (w>=x)&&(w<=y)) || ((w<=z)&&(w<=x)&&(w>=y)))
+  {
+      third = w;
+  }
+
+  if((w>=x)&&(w>=y)&&(w>=z)){
+      first = w;
+  }
+
+    if((w<=z)&&(w<=y)&&(w<=x)){
+    four = w;
+  }
+
+  if(((w>=z) && (w>=x) && (w<=y)) || ((w>=z) && (w<=x) && (w>=y)) || ((w<=z) && (z>=x) && (z>=y)))
+  {
+     second = w;
+}
+
+
+
+  if((x<=z)&&(x<=y)&&(x<=w)){
+    four = x;
+  }
+
+  if(((x>=w) && (x>=z) && (z<=y)) || ((x>=w) && (x<=z) && (z>=y)) || ((x<=w) && (x>=z) && (z>=y)))
+  {
+     second = x;
+
+  }
+
+  if(((x>=z)&&(x<=w)&&(x<=y)) || ((x<= z)&& (x>=w)&&(x<=y)) || ((x<=z)&&(x<=w)&&(x>=y)))
+  {
+      third = x;
+  }
+
+  if((x>=w)&&(x>=y)&&(x>=z)){
+      first = x;
+  }
+
+
+
+
+  if((y<=x)&&(y<=z)&&(y<=w)){
+    four = y;
+  }
+
+  if(((y>=w) && (y>=x) && (y<=z)) || ((y>=w) && (y<=x) && (y>=z)) || ((y<=w) && (y>=x) && (y>=z)))
+  {
+     second = y;
+  }
+
+     if((y>=x)&&(y>=w)&&(y>=z)){
+         first = y;
+     }
+     if(((y>=z)&&(y<=w)&&(y<=x)) || ((y<= z)&& (y>=w)&&(y<=x)) || ((y<=z)&&(y<=w)&&(y>=x)))
+     {
+         third = y;
+     }
+
+
+
+    /* Do not change the following output statement */
+    printf("%.1lf %.1lf %.1lf %.1lf\n",first, second, third, four);
+
+    return 0;
+}
