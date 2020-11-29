a99fa7fc7c0dbdbe069107ad54287f3f9f91f446
Brian Monoranu <Brian.monoranu@ucdconnect.com>
2020-11-29 22:25:27 +0000

    from lower case to Upper case

--- right.c
+++ right.c
@@ -14,17 +14,17 @@ fgets(Sentence, M , stdin);
 
 int i=0;
 char ch;
-while(i < M  && Sentence[i] != '\0' ){
+while(i < M  && Sentence[i] != '\0' ){//checks if it hasnt reached the null operator//
     ch = Sentence[i];
-    if( (ch >= 'a' && ch <= 'z' )){
+    if( (ch >= 'a' && ch <= 'z' )){//checkining if it is between a and z//
     Sentence[i] = (Sentence[i] - 32);
-
+//Convert to capital//
 }
 
 
 
   printf("%c", Sentence[i]);
-i++;
+i++;//check next figue//
 
 }
 //This do-while loop condition is checked last in this loop//
